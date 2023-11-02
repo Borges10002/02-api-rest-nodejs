5510e9c6fc65aa801f65139e93d0512e58d701d4
Borges10002 <borges10002@gmail.com>
2023-11-02 07:47:36 -0400

    udate

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import { app } from "./app";
 import { env } from "./env";
 
+const port = env.PORT;
+const host = "RENDER" in process.env ? "0.0.0.0" : "localhost";
+
 app
   .listen({
-    port: env.PORT,
+    port,
+    host,
   })
   .then(() => {
     console.log("HTTP Server Running!");
